7d211d36c8538fb885826255fb13206917ade6b0
Diogo <diogoascarneiro@hotmail.com>
2022-02-17 20:03:36 +0000

    First axios.get test

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 const Recipe = require("../models/Recipe.model");
+const axios = require('axios');
 
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  axios.get("/api/recipes/v2/a420ea59b1e9a0ec054859421027796b")
-  .then(response => Recipe.create({response}))
-  .catch(err => console.log(error));
-
-  res.render("index", {response}); 
+   axios.get("https://api.edamam.com/api/recipes/v2/96ae20daad99c35d721f1eb96ecdf9ba?type=public&app_id=5bf94e03&app_key=6505fc7507ee610a167d9c57a10c5cae")
+  .then(response => Recipe.create(response.data))
+  .catch(err => console.log(err));
+ 
+  res.render("index"); 
 });
 
 module.exports = router;
