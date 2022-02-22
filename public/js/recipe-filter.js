57a39ae1479fdd388a0e93565173121a94312d44
Diogo <diogoascarneiro@hotmail.com>
2022-02-22 15:01:38 +0000

    wip update on adding and removing filters on homepage

--- public/js/recipe-filter.js
+++ public/js/recipe-filter.js
@@ -7,15 +7,15 @@ $(document).ready(function(){
     dietRestSelect.change(() => {
         if (dietRestListArr.indexOf(dietRestSelect.val()) === -1) {
             dietRestListArr.push(dietRestSelect.val());
-            dietRestFilterListElem.append(`<p class="${dietRestSelect.val()}"><span class="remove-diet-restriction">X</span> ${dietRestSelect.children("option:selected").text()}</p>`);
+            dietRestFilterListElem.append(`<div class="${dietRestSelect.val()}"><span class="remove-diet-restriction">X</span> <span class="remove-diet-restriction-value">${dietRestSelect.children("option:selected").text()}</p></div>`);
         }        
     });
 
-    $("span.remove-diet-restriction").click(()=> {
-        console.log('CLICK :>> ');
-        let selectedOption = this.parent().attr('class');
+    dietRestFilterListElem.on("click", "span.remove-diet-restriction", ()=> {
+        let selectedOption = $("span.remove-diet-restriction").parent().attr("class");
+        $("span.remove-diet-restriction").parent().remove();
         console.log('selectedOption :>> ', selectedOption);
         console.log('dietRestListArr.indexOf(selectedOption) :>> ', dietRestListArr.indexOf(selectedOption));
-    })
+    });
 
   });
