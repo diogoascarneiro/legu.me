f045bcd1fc469da66802bd1962518032b3e0175e
Diogo Carneiro <diogoascarneiro@hotmail.com>
2022-03-04 12:25:13 +0000

    fixed a few more bugs in the filter

--- routes/index.js
+++ routes/index.js
@@ -70,7 +70,7 @@ if (filterData.ingredients.$lte != "" && filterData.ingredients.$gte != "") {
 /* Homepage Routes */
 
 router.get("/", (req, res, next) => {
-// recipeAPI.crawl(["a", "vegetarian", "Central Europe"], null);
+recipeAPI.crawl(["a", "vegetarian", undefined, undefined, "sweets"], null);
 
 Recipe.findRandom({}, {}, {limit:12}, function(err, foundRecipes) {
   if (!err) {
